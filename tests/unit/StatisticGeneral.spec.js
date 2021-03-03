7a53abb6d8e7c71d1f778e90298a6dc2279dea87
Greatman Lim <greatman.lim@mahti.org>
2021-03-03 17:43:27 +0200

    Add vue test library and utils

---
+++ tests/unit/StatisticGeneral.spec.js
@@ -0,0 +1,21 @@
+import { render } from '@testing-library/vue'
+import '@testing-library/jest-dom'
+import HelloWorld from '@/components/StatisticGeneral.vue'
+
+describe('StatisticGeneral.vue', () => {
+  it('renders statistic correctly', () => {
+    const statistic = {
+      follower: 14,
+      leader: 11,
+      paid: 22,
+      totalPaid: 1780,
+      totalAmount: 2200
+    }
+    const { getByText } = render(HelloWorld, {
+      props: { statistic }
+    })
+    expect(getByText('osallistujia: 25')).toBeInTheDocument()
+    expect(getByText('maksaneita: 22')).toBeInTheDocument()
+    expect(getByText('tuotot: 1780 € / 2200 €')).toBeInTheDocument()
+  })
+})
